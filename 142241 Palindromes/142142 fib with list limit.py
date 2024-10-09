a59866a114f05acb2c4e17b858d055dc4ed9722e
Chathura madusanka <63940398+ChathuraGH@users.noreply.github.com>
2024-10-09 20:40:04 +0530

    source files were added.

---
+++ 142241 Palindromes/142142 fib with list limit.py
@@ -0,0 +1,101 @@
+import re
+u=int(input("start point of range :- "))
+uu=int(input("end point of range :- "))
+a=[]
+uu=uu+1
+
+def num(u,uu):
+    for i in range(u,uu):
+        a.append(u)
+        u+=1
+    #print(a)
+
+
+def p():
+    for e in a:
+        x=e
+        xx=str(x)
+        xxx=len(xx)
+        if xxx==2 or xxx==3:
+            b=[]
+            for c in xx:
+                b.append(c)
+                
+            if b[0] == b[xxx-1]:
+                print(e,"yes")
+            else:
+                pass
+                
+        elif xxx==4 or xxx==5:
+            b=[]
+            for c in xx:
+                b.append(c)
+            
+            if b[0] == b[xxx-2] and b[1] == b[xxx-1]:
+                print(e,"yes")
+            else:
+                pass
+        elif xxx==6 or xxx==7:
+            b=[]
+            for c in xx:
+                b.append(c)
+            
+            if b[0] == b[xxx-3] and b[1] == b[xxx-2] and b[2] == b[xxx-1]:
+                print(e,"yes")
+            else:
+                pass
+        elif xxx==8 or xxx==9:
+            b=[]
+            for c in xx:
+                b.append(c)
+            
+            if b[0] == b[xxx-4] and b[1] == b[xxx-3] and b[2] == b[xxx-2] and b[3] == b[xxx-1]:
+                print(e,"yes")
+            else:
+                pass
+        elif xxx==10 or xxx==11:
+            b=[]
+            for c in xx:
+                b.append(c)
+            
+            if b[0] == b[xxx-5] and b[1] == b[xxx-4] and b[2] == b[xxx-3] and b[3] == b[xxx-2] and b[4] == b[xxx-1]:
+                print(e,"yes")
+            else:
+                pass
+        else:
+            #print(e,"is s")
+            pass
+            
+def e():
+    a=input('"press Enter to exit"')
+    exit
+    
+num(u,uu)
+p()
+
+e()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 142241 Palindromes/142142 fib with out list limit.py
@@ -0,0 +1,117 @@
+u=int(input("start point of range :- "))
+uu=int(input("end point of range :- "))
+
+uu=uu+1
+
+def num(u,uu):
+    for i in range(u,uu):
+        try:
+            x=u
+            xx=str(x)
+            xxx=len(xx)
+            if xxx==2 or xxx==3:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                    
+                if b[0] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass
+                    
+            elif xxx==4 or xxx==5:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                
+                if b[0] == b[xxx-2] and b[1] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass
+            elif xxx==6 or xxx==7:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                
+                if b[0] == b[xxx-3] and b[1] == b[xxx-2] and b[2] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass
+            elif xxx==8 or xxx==9:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                
+                if b[0] == b[xxx-4] and b[1] == b[xxx-3] and b[2] == b[xxx-2] and b[3] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass
+            elif xxx==10 or xxx==11:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                
+                if b[0] == b[xxx-5] and b[1] == b[xxx-4] and b[2] == b[xxx-3] and b[3] == b[xxx-2] and b[4] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass
+
+            elif xxx==12 or xxx==13:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                
+                if b[0] == b[xxx-6] and b[1] == b[xxx-5] and b[2] == b[xxx-4] and b[3] == b[xxx-3] and b[4] == b[xxx-2] and b[5] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass
+            elif xxx==14 or xxx==15:
+                b=[]
+                for c in xx:
+                    b.append(c)
+                
+                if b[0] == b[xxx-7] and b[1] == b[xxx-6] and b[2] == b[xxx-5] and b[3] == b[xxx-4] and b[4] == b[xxx-3] and b[5] == b[xxx-2] and b[6] == b[xxx-1]:
+                    print(u,"yes")
+                else:
+                    pass     
+            else:
+                #print(e,"is s")
+                pass
+        except:
+            print("exception occured")
+            pass
+        u+=1
+            
+def e():
+    a=input('"press Enter to exit"')
+    exit
+    
+num(u,uu)
+
+
+e()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
